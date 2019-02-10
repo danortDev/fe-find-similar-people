183ae43045c8d9629efdd2f8cc88ef7c4dd90073
Daniel Ortiz Lira <d.ortiz.lira@gmail.com>
2019-02-09 20:45:34 -0500

    add initial palette

---
+++ src/app/palette.js/index.js
@@ -0,0 +1,16 @@
+import tinycolor from 'tinycolor2';
+
+const COLOURS = {
+  PRIMARY: '##00ACC1',
+  GREY: '#F5F5F5',
+  BLACK: '#000000',
+  WHITE: '#FFFFFF',
+  TRANSPARENT: tinycolor({ r: 0, g: 0, b: 0, a: 0 })
+};
+
+const getColor = (colorName) => {
+  const color = COLOURS[colorName] || COLOURS['PRIMARY'];
+  return tinycolor(color).clone();
+};
+
+export default getColor;
